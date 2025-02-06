2bb8f85c5644da7ffd4b9f6aa7d2c046fec46700
Ashish Kumar <ashishkumar748867@gmail.com>
2025-02-06 23:47:11 +0530

    fix state manegment

--- src/component/StatusCard.jsx
+++ src/component/StatusCard.jsx
@@ -9,7 +9,7 @@ export const StatusCard = () => {
   const dispatch = useDispatch();
    const [username, setusername] = useState("");
    const handleChange = (e) => {
-    setusername(e.target.value);
+    setusername(e.target.value); 
   };
   const handleClick = () => {
     dispatch(fetchData(username));
@@ -19,48 +19,57 @@ export const StatusCard = () => {
  
   return (
     <div className="container">
-      <div className="search-div">
-        <input
-          type="text"
-          placeholder="Enter your name"
-          className="input-box"
-          onChange={(e) => handleChange(e)}
-        />
-        <button className="btn" onClick={handleClick}>
-          Submit
-        </button>
-      </div>
-      <div className="problem-data">
-        <div className="box">
-          {myData.easySolved}/{myData.totalEasy} <br />
-          easy
-        </div>
-        <div className="box">
-          {myData.mediumSolved}/{myData.totalMedium} <br />
-          mid
-        </div>
-        <div className="box">
-          {myData.hardSolved}/{myData.totalHard} <br />
-          hard
-        </div>
-      </div>
-      <p>totalSolved:{myData.totalSolved}</p>
-      <div className="details-data">
-        <div className="details-box">
-          {myData.acceptanceRate} <br />
-          acceptanceRate
-        </div>
-        <div className="details-box">
-          {myData.contributionPoints}
-          <br />
-          contribution
-        </div>
-        <div className="details-box">
-          {myData.ranking}
-          <br />
-          ranking
-        </div>
-      </div>
+    <div className="search-div">
+      <input
+        type="text"
+        placeholder="Enter your name"
+        className="input-box"
+        onChange={handleChange}
+      />
+      <button className="btn" onClick={handleClick}>
+        Submit
+      </button>
     </div>
+
+    {isLoading ? (
+      <p>Loading...</p>
+    ) : myData ? (
+      <>
+        <div className="problem-data">
+          <div className="box">
+            {myData.easySolved}/{myData.totalEasy} <br />
+            easy
+          </div>
+          <div className="box">
+            {myData.mediumSolved}/{myData.totalMedium} <br />
+            mid
+          </div>
+          <div className="box">
+            {myData.hardSolved}/{myData.totalHard} <br />
+            hard
+          </div>
+        </div>
+        <p>totalSolved: {myData.totalSolved}</p>
+        <div className="details-data">
+          <div className="details-box">
+            {myData.acceptanceRate} <br />
+            acceptanceRate
+          </div>
+          <div className="details-box">
+            {myData.contributionPoints}
+            <br />
+            contribution
+          </div>
+          <div className="details-box">
+            {myData.ranking}
+            <br />
+            ranking
+          </div>
+        </div>
+      </>
+    ) : (
+      <p>No data available</p>
+    )}
+  </div>
   );
 };
